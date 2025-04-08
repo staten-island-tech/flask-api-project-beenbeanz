378178df23e64e85b22bd33fcfa122804e2a4524
beenbeanz <haileylan404@gmail.com>
2025-04-08 09:26:44 -0400

    a

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, requests
+from flask import Flask, render_template
+import requests
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    
+    response = requests.get("https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date_display,main_reference_numb&page=1&limit=100")
+    data = response.json()
+    artworkList = data["data"]
+    return render_template("index.html", artworkList=artworkList)
+app.run(debug=True)
 
