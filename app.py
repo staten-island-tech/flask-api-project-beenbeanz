82e383c7b1d854a5c6992f110ccd2a623fd58ba7
beenbeanz <haileylan404@gmail.com>
2025-04-09 09:26:23 -0400

    a

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ def home():
     response = requests.get("https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date_display,main_reference_numb&page=1&limit=100")
     data = response.json()
     artworkList = data["data"]
+    
     return render_template("index.html", artworkList=artworkList)
 app.run(debug=True)
 
