9e13c4884e39ac0e8b1380b751b3fb943a4ae59d
beenbeanz <haileylan404@gmail.com>
2025-04-11 09:27:01 -0400

    a

--- app.py
+++ app.py
@@ -4,15 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    response = requests.get("https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date_display,main_reference_numb&page=1&limit=10")
+    response = requests.get("https://api.artic.edu/api/v1/artworks?fields=id,title,image_id,artist_display,date_display,main_reference_numb&page=1&limit=10")
     data = response.json()
     artworkList = data["data"]
-    for artwork in artworkList:
-        Url = requests.get(f"https://api.artic.edu/api/v1/artworks/{artwork["id"]}?fields=id,title,image_id")
-        imageData = Url.json()
-        id = imageData["data"]["image_id"]
-        iiifUrl = imageData["config"]["iiif_url"]
-        imageUrl = iiifUrl.append(id)
-    return render_template("index.html", artworkList=artworkList, imageUrl = imageUrl)
+    return render_template("index.html", artworkList=artworkList)
 app.run(debug=True)
 
