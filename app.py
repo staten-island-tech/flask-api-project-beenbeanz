fea8f5187de086fc010fe6ae8b1930d56d396c00
beenbeanz <haileylan404@gmail.com>
2025-04-29 09:09:55 -0400

    a

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    response = requests.get("https://api.artic.edu/api/v1/artworks?fields=id,title,image_id,artist_display,date_display,main_reference_numb&page=1&limit=10")
+    response = requests.get("https://api.artic.edu/api/v1/artworks?fields=id,title,image_id,artist_display,date_display,main_reference_numb&page=1&limit=50")
     data = response.json()
     artworkList = data["data"]
     return render_template("index.html", artworkList=artworkList)
@@ -20,13 +20,21 @@ def artworkDetail(id):
     origin = artworkData.get('place_of_origin')
     imageId = artworkData.get('image_id')
     date = artworkData.get('date_start')
+    description = artworkData.get('short_description')
+    if description == None:
+        description = ''
+    medium = artworkData.get('medium_display')
+    department = artworkData.get('department_title') 
     imageUrl = f"https://www.artic.edu/iiif/2/{imageId}/full/843,/0/default.jpg" 
     return render_template("artwork.html", artwork = {
         'title': title, 
         'artist': artist, 
         'origin': origin,
         'image' : imageUrl,
-        'date': date
+        'date': date,
+        'description': description,
+        'medium': medium,
+        'department': department,
     })
 if __name__ == '__main__':
     app.run(debug=True)
