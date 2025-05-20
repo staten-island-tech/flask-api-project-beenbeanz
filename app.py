5a0e68d9fe5cc1ff73659059b4153e628471b6a3
beenbeanz <haileylan404@gmail.com>
2025-05-20 09:26:26 -0400

    g

--- app.py
+++ app.py
@@ -4,28 +4,36 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    response = requests.get("https://api.artic.edu/api/v1/artworks?fields=id,title,image_id,artist_display,date_display,main_reference_numb&page=1&limit=50")
-    data = response.json()
-    artworkList = data["data"]
+    try:
+        response = requests.get("https://api.artic.edu/api/v1/artworks?fields=id,title,image_id,artist_display,date_display,main_reference_numb&page=1&limit=50")
+        data = response.json()
+    except:
+        print("Artwork not available.")
+    else:
+        artworkList = data["data"]
     return render_template("index.html", artworkList=artworkList)
 
 @app.route("/artworks/<int:id>")
 def artworkDetail(id):
-    response = requests.get(f"https://api.artic.edu/api/v1/artworks/{id}")
-    data = response.json()
-    artworkData = data.get("data", {})
+    try:
+        response = requests.get(f"https://api.artic.edu/api/v1/artworks/{id}")
+        data = response.json()
+    except:
+        print("Artwork data not available")
+    else:
+        artworkData = data.get("data", {})
 
-    title = artworkData.get('title')
-    artist = artworkData.get('artist_display')
-    origin = artworkData.get('place_of_origin')
-    imageId = artworkData.get('image_id')
-    date = artworkData.get('date_start')
-    description = artworkData.get('short_description')
-    if description == None:
-        description = ''
-    medium = artworkData.get('medium_display')
-    department = artworkData.get('department_title') 
-    imageUrl = f"https://www.artic.edu/iiif/2/{imageId}/full/843,/0/default.jpg" 
+        title = artworkData.get('title')
+        artist = artworkData.get('artist_display')
+        origin = artworkData.get('place_of_origin')
+        imageId = artworkData.get('image_id')
+        date = artworkData.get('date_start')
+        description = artworkData.get('short_description')
+        if description == None:
+            description = ''
+        medium = artworkData.get('medium_display')
+        department = artworkData.get('department_title') 
+        imageUrl = f"https://www.artic.edu/iiif/2/{imageId}/full/843,/0/default.jpg" 
     return render_template("artwork.html", artwork = {
         'title': title, 
         'artist': artist, 
@@ -34,7 +42,22 @@ def artworkDetail(id):
         'date': date,
         'description': description,
         'medium': medium,
-        'department': department,
+        'department': department
     })
+'''@app.route("/artworks/<int:date>")
+def artworkFilter():
+    try:
+        response = requests.get(f"https://api.artic.edu/api/v1/artworks")
+        data = response.json()
+        artworkList = data["data"]
+    except:
+        print("Artwork data not available")
+    else:
+        for artwork in artworkList:
+            artworkData = artwork.get("data", {})
+            date = artworkData.get('date_start')
+            datesArr = []
+            datesArr.append(date)
+    return render_template("index.html", date=date)'''
 if __name__ == '__main__':
     app.run(debug=True)
